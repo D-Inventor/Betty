7397bfe94351851f5c9317e4b915d05e59ef7e21
D_Inventor <D.Heutinck@students.uu.nl>
2019-01-23 08:29:52 +0100

    Created new project template and configured all dependencies

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace betty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Internet of Things!");
+        }
+    }
+}
